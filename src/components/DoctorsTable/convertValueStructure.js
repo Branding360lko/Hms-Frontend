46a4c56198857558141f136a16f32a6a8acf98f6
Syed Fahad Junaid <syedfahadjunaid@gmail.com>
2024-06-26 15:55:23 +0530

    commit deleted

--- src/components/DoctorsTable/convertValueStructure.js
+++
@@ -1,9 +0,0 @@
-export const convertValue = (array) => {
-  const changevalue = [];
-  for (let index = 0; index < array?.length; index++) {
-    const label = array[index]?.Name;
-    const value = array[index]?._id;
-    changevalue.push({ label, value });
-  }
-  return changevalue;
-};
